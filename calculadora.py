4b19d8ed0d0af3cb3eca17291228230700b82dee
stiv19 <stevencas0002@gmail.com>
2025-02-17 11:18:44 -0500

    Este archivo tiene las operaciones de suma,resta,multip´licación y division de la calculadora basica

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+print(":: CALCULADORA BASICA ::")
+num1=20
+num2=4
+suma=num1 + num2
+resta=num1 - num2
+div=num1/num2
+mul=num1*num2
+
+print("La suma de los dos numeros es: ", suma)
+print("La resta de los dos numeros es: ", resta)
+print("La division de los dos numeros es: ", div)
+print("La multiplicacion de los dos numeros es: ", mul)
